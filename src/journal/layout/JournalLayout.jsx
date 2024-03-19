cd78f0c9d596934e03914f4dbcce857fa805f25f
Christina Lorenzo <christinalorenzo@gmail.com>
2024-03-19 21:53:23 +0100

    add sideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,15 @@
 import { Box } from "@mui/material";
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
+
+const drawerWidth = 280;
+
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      <NavBar />
+      <NavBar drawerWidth={ drawerWidth } />
+
+      <SideBar drawerWidth={ drawerWidth } />
       <Box
         component='main'
         sx={{ flexGrow: 1, p: 3 }}
